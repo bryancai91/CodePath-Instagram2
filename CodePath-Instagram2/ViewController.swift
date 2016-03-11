4871474cbc1486e522d70695ab1f9607cc89cf24
Bryan Cai <caibryan@wustl.edu>
2016-03-11 02:47:11 -0600

    Initial Commit

---
+++ CodePath-Instagram2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodePath-Instagram2
+//
+//  Created by Bryan Cai on 3/11/16.
+//  Copyright © 2016 Bryan Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
